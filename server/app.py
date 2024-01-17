ecf4c552ac9e1898e0448308b22fc62bdea2322a
aminafarah <amishkamohamed563@gmail.com>
2024-01-17 19:10:52 +0300

    done

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource,reqparse
 
 from models import db, Plant
 
@@ -17,38 +17,89 @@ db.init_app(app)
 api = Api(app)
 
 
-class Plants(Resource):
+class PlantsResource(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument("name", required=True, help="Name cannot be blank", type=str)
+    parser.add_argument("image", required=True, help="image cannot be blank", type=str)
+    parser.add_argument("price", required=True, help="price cannot be blank", type=int)
+
 
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
+      
+        args = PlantsResource.parser.parse_args()
+        name = args.get('name')
+        image = args.get('image')
+        price = args.get('price')
+        to_add = Plant(name=name, image=image, price=price)
+        
 
 
-api.add_resource(Plants, '/plants')
+        try:
+            db.session.add(to_add)
+            db.session.commit()
+            return { 'new_plant' :{
+                "id" : to_add.id,
+                "name": to_add.name,
+                "image": to_add.image,
+                "price": to_add.price
+            }}, 201
+        except Exception as e:
+            db.session.rollback()
+            return {
+                'error' : str(e)
+            }, 500
+        finally:
+            db.session.close()
 
 
-class PlantByID(Resource):
+#         new_plant =Plant(
+#             name=data['name'],
+#             image=data['image'],
+#             price=data['price']
+#         )
+#         db.session.add(new_plant)
+#         db.session.commit()
 
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+
+#         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+api.add_resource(PlantsResource, '/plants')
+
+
+# class PlantByID(Resource):
+
+#     def get(self, id):
+#         plant = Plant.query.filter_by(id=id).first().to_dict()
+#         return make_response(jsonify(plant), 200)
+#     def post(self,id):
+#         data=request.get_json()
+#         plant=Plant.query.filter_by(id=id).first()
+
+#         for attr in data:
+#             setattr(plant,attr,data[attr])
+#         db.session.add(plant)
+#         db.session.commit()
+
+#         return make_response(plant.to_dict(), 200) 
+    
+#     def delete(self,id):
+#         plant=Plant.query.filter_by(id=id).first()
+#         db.session.delete(plant)
+#         db.session.commit()
+        
+
+#         return make_response('',204)
+    
+
+
+
+#api.add_resource(PlantByID, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
